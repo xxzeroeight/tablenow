3f0a91281779418714568c7449a057948ae28f83
xxzeroeight <xxzeroeight@naver.com>
2026-04-01 15:10:48 +0900

    fix: Security Config, Auth 엔드포인트 수정

--- src/main/java/com/tablenow/tablenow/global/config/SecurityConfig.java
+++ src/main/java/com/tablenow/tablenow/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -89,8 +90,8 @@ public class SecurityConfig
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
             // 5. HTTP 요청 권한 설정
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/login", "/api/auth/signup", "/api/auth/reissue").permitAll()
-                    .requestMatchers("/api/auth/logout").authenticated()
+                    .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/signup", "/api/auth/reissue").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/auth/logout").authenticated()
                     .anyRequest().authenticated()
             );
 
