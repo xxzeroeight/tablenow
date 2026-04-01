d25127bcdc3008f293022e400bb40a5f0c895d8c
xxzeroeight <xxzeroeight@naver.com>
2026-04-01 15:09:04 +0900

    fix: 동시 재발급 문제 수정

--- src/main/java/com/tablenow/tablenow/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/tablenow/tablenow/domain/auth/repository/RefreshTokenRepository.java
@@ -2,7 +2,9 @@ package com.tablenow.tablenow.domain.auth.repository;
 
 import com.tablenow.tablenow.domain.auth.entity.RefreshToken;
 import com.tablenow.tablenow.domain.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID>
 {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<RefreshToken> findByToken(String token);
     void deleteByUser(User user);
     Optional<RefreshToken> findByUser(User user);
