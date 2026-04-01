686498626bd81e47c41c58c8de14eda064f892f3
xxzeroeight <xxzeroeight@naver.com>
2026-04-01 13:42:36 +0900

    feat: RefreshToken 도메인 추가

---
+++ src/main/resources/db/migration/V2__create_refresh_tokens.sql
@@ -0,0 +1,9 @@
+CREATE TABLE refresh_tokens
+(
+    id          UUID         PRIMARY KEY NOT NULL,
+    token       TEXT                     NOT NULL,
+    user_id     UUID                     NOT NULL UNIQUE,
+    created_at  TIMESTAMPTZ              NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_refresh_tokens_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
